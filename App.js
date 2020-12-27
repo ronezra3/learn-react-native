807eaf91c0aa5b3d2a057e3f9e5988db22f2557b
ronezra3 <ronezra@rons-MacBook-Pro.local>
2020-12-27 15:43:38 +0200

    textInput + Button

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!!!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
+    const [text, setText] = React.useState('Useless Placeholder');
+
+
+    function fn() {
+      console.log(text)
+    }
+
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={ { height: 40, borderColor: 'gray', borderWidth: 1 } }
+          onChangeText={inputValue => {setText(inputValue)}}
+          value={text}
+        />
+
+        <Button title="click" onPress={fn} />
+        <View>
+          <Text>Hello</Text>
+        </View>
+        <Text>Open up App.js to start working on your app!!!!!</Text>
+      </View>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
