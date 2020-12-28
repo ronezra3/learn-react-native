339e4357c333e25ef90330600cc0fa9def46f994
ronezra3 <ronezra@rons-MacBook-Pro.local>
2020-12-28 11:02:57 +0200

    flex-box backup

--- App.js
+++ App.js
@@ -1,40 +1,49 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Image } from 'react-native';
 
 export default function App() {
 
 
 
 
-    const [text, setText] = React.useState('Useless Placeholder');
+  const [text, setText] = React.useState('Useless Placeholder');
 
 
-    function fn() {
-      console.log(text)
-    }
-
-    return (
-      <View style={styles.container}>
-        <TextInput
-          style={ { height: 40, borderColor: 'gray', borderWidth: 1 } }
-          onChangeText={inputValue => {setText(inputValue)}}
-          value={text}
-        />
-
-        <Button title="click" onPress={fn} />
-        <View>
-          <Text>Hello</Text>
-        </View>
-        <Text>Open up App.js to start working on your app!!!!!</Text>
-      </View>
-    );
+  function fn() {
+    console.log(text)
   }
 
-  const styles = StyleSheet.create({
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={inputValue => { setText(inputValue) }}
+        value={text}
+      />
+
+      <Image source={require('./cat.jpg')} style={styles.image} />
+      <Button title="click" onPress={fn} />
+      <View>
+        <Text>Hello</Text>
+      </View>
+      <Text>Open up App.js to start working on your app!!!!!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create(
+  {
+
     container: {
       flex: 1,
       backgroundColor: '#fff',
       alignItems: 'center',
-      justifyContent: 'center',
+      justifyContent: 'space-around',
+
     },
-  });
+
+    image: {
+      height: 100, width: 100
+    }
+  }
+);
